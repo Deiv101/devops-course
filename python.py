dbccada68aabbaf36c7992735d1eb43793264ec5
Deiv101 <hrakojoana@yahoo.com>
2021-03-17 12:23:10 -0400

    Added Python Code

--- python.py
+++ python.py
@@ -1,1 +1,15 @@
-Python Folder 
+for i in [a, e, i, o, u, u, o, i, e, a]:
+ print(count(1)def greet(name):
+    """
+    This function greets to
+    the person passed in as
+    a parameter
+    """
+    print("Hello, " + name + ". Good morning!")
+
+greet('Deiv')
+
+############################################################
+for x in ['a', 'e', 'i', 'o', 'u', 'u', 'o', 'i', 'e', 'a']:
+ print((x), "\t")
+ 
